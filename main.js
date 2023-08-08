1cad25b2e1f92ccaa831d685b26cd636bebc814e
Yannick Kartodikromo <yannickkartodikromo@gmail.com>
2023-08-07 21:03:29 -0300

    added canvas

---
+++ main.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("canvas");
+
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+canvas.width = window_height;
+canvas.height = window_width;
+
+canvas.style.background = "black";
