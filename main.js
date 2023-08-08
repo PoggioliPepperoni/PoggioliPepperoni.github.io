5f45c1b6b2b8c15aa54efddb354161437ed480a5
Yannick Kartodikromo <yannickkartodikromo@gmail.com>
2023-08-07 22:25:26 -0300

    added canvas

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
 let canvas = document.getElementById("canvas");
 
-var window_height = window.innerHeight;
-var window_width = window.innerWidth;
+let context = canvas.getContext("2d");
 
-canvas.width = window_height;
-canvas.height = window_width;
+canvas.style.background = "white";
 
-canvas.style.background = "black";
+context.fillStyle = "black";
+
+context.fillRect(100,0,100,100);
+context.fillRect(300,0,100,100);
+context.fillRect(500,0,100,100);
+context.fillRect(700,0,100,100);
+
+context.fillRect(0,100,100,100);
+context.fillRect(200,100,100,100);
+context.fillRect(400,100,100,100);
+context.fillRect(600,100,100,100);
+
+context.fillRect(100,200,100,100);
+context.fillRect(300,200,100,100);
+context.fillRect(500,200,100,100);
+context.fillRect(700,200,100,100);
+
+context.fillRect(0,300,100,100);
+context.fillRect(200,300,100,100);
+context.fillRect(400,300,100,100);
+context.fillRect(600,300,100,100);
+
+context.fillRect(100,400,100,100);
+context.fillRect(300,400,100,100);
+context.fillRect(500,400,100,100);
+context.fillRect(700,400,100,100);
+
+context.fillRect(0,500,100,100);
+context.fillRect(200,500,100,100);
+context.fillRect(400,500,100,100);
+context.fillRect(600,500,100,100);
+
+context.fillRect(100,600,100,100);
+context.fillRect(300,600,100,100);
+context.fillRect(500,600,100,100);
+context.fillRect(700,600,100,100);
+
+context.fillRect(0,700,100,100);
+context.fillRect(200,700,100,100);
+context.fillRect(400,700,100,100);
+context.fillRect(600,700,100,100);
